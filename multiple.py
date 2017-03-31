1254a3b5b95e9ab89dede72222bdcad90d050245
apolaris <beiczhang@gmail.com>
2017-03-31 11:53:04 -0400

    Add files via upload

--- multiple.py
+++ multiple.py
@@ -3,46 +3,8 @@ def match (a, b):
     if a == b:
         return 0
 
-    else
+    else:
         return -p
-
-
-
-def pariwise (a, b):
-    global gap
-    global mis
-    alen = len(a)
-    blen = len(b)
-    col1 = [0]*(alen+1);
-    col2 = [0]*(alen+1);
-    col1[0] = 0
-    for i in range(1,alen+1)
-        col1[i] = col1[i-1] - gap;
-    now = 2
-    for i in range(0,blen)
-        if(now == 1)
-            col1 = col2[0] - gap
-            for j in range(1,alen+1)
-                col1[j] = max(col1[j-1] - gap, col2[j] - gap, col2[j-1] + match(a[j-1], b[i]))
-            now = 2
-        else if(now == 2)
-            col2 = col1[0] - gap
-            for j in range(1,alen+1)
-                col2[j] = max(col2[j-1] - gap, col1[j] - gap, col1[j-1] + match(a[j-1], b[i]))
-            now = 1
-    if(now == 1)
-        return col2[alen]
-    else
-        return col1[alen]
-
-
-
-def build_tree (deff, num):
-#deff is the difference between the sequences
-    score = [[0]*num]*num
-    for i in range(0,num)
-        for j in range(0,num)
-            score[i][j] += pairwise(seqs[i], seqs[j])
 
 
 
@@ -74,7 +36,7 @@ def build_tree (deff, num):
 
 class sequences:
     'sequence list for alignment'
-    def __init__(self, seqs, a, b, mis):
+    def _0_init__(self, seqs, a, b, mis):
         self.seqs = seqs
         self.num = len(seqs)
         self.a = a
@@ -90,10 +52,10 @@ class sequences:
     #calculate the difference
     def init_matrix (self):
         self.score = []
-        for i in range(0,self.num)
+        for i in range(0,self.num):
         self.score.append([0]*self.num)
         for i in range(0,self.num):
-            node = [1,[],[],[seqs[i]],[],-1,-1] #num, left, right, seqs, weight, distance from parent, height
+            node = [1,[],[],[seqs[i]],[],-1,-1,0] #num, left, right, seqs, weight, distance from parent, height, size
             self.score[i].append(node)
             for j in range(0,i):
                 self.score[i][j] = self.pairwise(i,j)
@@ -145,19 +107,19 @@ class sequences:
                 key = 2
         if (key==2):
             return min(M1[num1], D1[num1], I1[num1])
-        else
+        else:
             return min(M2[num1], D2[num1], I2[num1])
 
 
     def buildtree (self):
 
         num = self.num
-        diff = self.score1
+        diff = self.score
         d = 0
         di = -1
         dj = -1
         nj = []
-        for i in range(0,num)
+        for i in range(0,num):
             nj.append([0]*num)
         while(num>=2):
             for i in range(0,num):
@@ -172,7 +134,7 @@ class sequences:
 
             diff[i][num][3] = 0.5*diff[i][j]+(su[i] - su[j])/(2*(num-2))
             diff[j][num][3] = diff[i][j] - diff[i][num][3]
-            node = [0,diff[i][num],diff[j][num],[],-1,-1]
+            node = [0,diff[i][num],diff[j][num],[],-1,-1,0]
             diffk = []
             for i in range(0,num):
                 if(i!=di and i != dj):
@@ -192,16 +154,33 @@ class sequences:
                 diff[i].insert(num-2,diffk[i])
             diff.insert(num-2, diffk)
             num=num-1
-        self.weighttree(diff[1],1,[])
-        self.tree = diff[1]
+        self.weighttree(diff[0][1],1,[])
+        self.tree = diff[0][1]
         return diff[1]
+
+    def sizeoftree (self , t):
+        if(not t[1] and not t[2]):
+            t[7] = 1
+            return 1
+        if(not t[1] and t[2]):
+            size = sizeoftree(t[2]) +1
+            t[7] = size
+            return size
+        if(not t[2] and t[1]):
+            size = sizeoftree(t[1]) +1
+            t[7] = size
+            return size
+        size = sizeoftree(t[1]) + sizeoftree(t[2]) + 1
+        t[7] = size
+        return size
     def weighttree (self,t,h,dif):
+        sizeoftree(t)
         t[6] = h
         if(not dif):
             dif.append(0)
         else:
             dif.append(t[5])
-        if(t[1]):
+        if(t[1] and (not t[2])):
             weighttree(t[1],h+1,dif)
         if(t[2]):
             weighttree(t[2],h+1,dif)
